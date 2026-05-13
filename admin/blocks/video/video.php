11c02ebb6b04c10fcf20d01a035819317a763657
Thomas Pericoi <thomaspericoi@gmail.com>
2026-05-13 10:37:52 +0200

    Add structured data

--- admin/blocks/video/video.php
+++ admin/blocks/video/video.php
@@ -39,9 +39,55 @@ if (!empty($block['className'])) {
 
 $styles = array("");
 $style  = implode('; ', $styles);
+
+/* Schema.org */
+$video_schema = null;
+
+if ($media && ($video || $youtube)) {
+    $video_name = $title ? wp_strip_all_tags($title) : get_the_title($post_id);
+    $video_description = $text ? wp_strip_all_tags($text) : get_the_excerpt($post_id);
+
+    $thumbnail_url = null;
+
+    if ($video_poster && !empty($video_poster['url'])) {
+        $thumbnail_url = $video_poster['url'];
+    } elseif ($img && !empty($img['url'])) {
+        $thumbnail_url = $img['url'];
+    } elseif (has_post_thumbnail($post_id)) {
+        $thumbnail_url = get_the_post_thumbnail_url($post_id, 'full');
+    } elseif ($youtube) {
+        $thumbnail_url = 'https://img.youtube.com/vi/' . $youtube . '/maxresdefault.jpg';
+    }
+
+    if ($video_name && $video_description && $thumbnail_url) {
+        $video_schema = [
+            '@context' => 'https://schema.org',
+            '@type' => 'VideoObject',
+            'name' => $video_name,
+            'description' => $video_description,
+            'thumbnailUrl' => [$thumbnail_url],
+            'uploadDate' => get_the_date('c', $post_id),
+        ];
+
+        if ($media === 'youtube' && $youtube) {
+            $video_schema['embedUrl'] = 'https://www.youtube.com/embed/' . $youtube;
+            $video_schema['contentUrl'] = 'https://www.youtube.com/watch?v=' . $youtube;
+        }
+
+        if ($media === 'video' && $video) {
+            $video_schema['contentUrl'] = $video;
+        }
+    }
+}
 ?>
 
 <!-- Block - Video -->
+<?php if ($video_schema) : ?>
+    <script type="application/ld+json">
+        <?php echo wp_json_encode($video_schema, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES); ?>
+    </script>
+<?php endif; ?>
+
 <section class="<?php echo esc_attr($classes); ?>" style="<?php echo esc_attr($style); ?>">
     <div class="container container-sm">
         <div class="introduction">
--- template-parts/faq.php
+++ template-parts/faq.php
@@ -1,24 +1,82 @@
+<?php
+$faq_schema = null;
+
+if (!empty($args['faq'])) {
+
+    $main_entity = [];
+
+    foreach ($args['faq'] as $faq) {
+
+        if (empty($faq['title']) || empty($faq['text'])) {
+            continue;
+        }
+
+        $main_entity[] = [
+            '@type' => 'Question',
+            'name' => wp_strip_all_tags($faq['title']),
+            'acceptedAnswer' => [
+                '@type' => 'Answer',
+                'text' => wp_strip_all_tags($faq['text']),
+            ],
+        ];
+    }
+
+    if (!empty($main_entity)) {
+        $faq_schema = [
+            '@context' => 'https://schema.org',
+            '@type' => 'FAQPage',
+            'mainEntity' => $main_entity,
+        ];
+    }
+}
+?>
+
+<?php if ($faq_schema) : ?>
+    <script type="application/ld+json">
+        <?php echo wp_json_encode(
+            $faq_schema,
+            JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES
+        ); ?>
+    </script>
+<?php endif; ?>
+
 <!-- F.A.Q. -->
 <section id="faq" class="faq-block">
     <div class="container">
-        <?php if ($args["title"]) : ?>
-            <h2><?php echo $args["title"]; ?></h2>
+
+        <?php if (!empty($args["title"])) : ?>
+            <h2>
+                <?php echo esc_html($args["title"]); ?>
+            </h2>
         <?php endif; ?>
-        <?php if ($args["faq"]) : ?>
+
+        <?php if (!empty($args["faq"])) : ?>
             <ul class="faq-list">
+
                 <?php foreach ($args["faq"] as $faq) : ?>
-                    <details class="faq">
-                        <summary>
-                            <h3 class="h4-size"><?php echo $faq['title']; ?></h3>
-                        </summary>
-                        <?php if ($faq['text']) : ?>
-                            <div class="content formatted">
-                                <?php echo $faq['text']; ?>
-                            </div>
-                        <?php endif; ?>
-                    </details>
+
+                    <li>
+                        <details class="faq">
+
+                            <summary>
+                                <h3 class="h4-size">
+                                    <?php echo esc_html($faq['title']); ?>
+                                </h3>
+                            </summary>
+
+                            <?php if (!empty($faq['text'])) : ?>
+                                <div class="content formatted">
+                                    <?php echo wp_kses_post($faq['text']); ?>
+                                </div>
+                            <?php endif; ?>
+
+                        </details>
+                    </li>
+
                 <?php endforeach; ?>
+
             </ul>
         <?php endif; ?>
+
     </div>
 </section>
